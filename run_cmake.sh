2bb3e417c1c91c460b4b9a4e6e1fc81fa4618527
James Moran <jojo.2nd.jm@gmail.com>
2018-04-23 20:31:41 +0100

    Replace default build system on linux

--- run_cmake.sh
+++ run_cmake.sh
@@ -1,4 +1,8 @@
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -G "Unix Makefiles" build
+
+cd build/linux
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -G "Ninja" ../..
+cd ../../
+
 # cmake -DCMAKE_BUILD_TYPE=Debug -G "Unix Makefiles" build
-mv ./build/compile_commands.json ./compile_commands.json
+mv ./build/linux/compile_commands.json ./compile_commands.json
 
